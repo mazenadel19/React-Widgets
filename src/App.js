df238050586a2c6df4be2392d2ede4441690556f
Mazen <mazenadel19@gmail.com>
2021-06-03 01:29:04 +0200

    accordion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import Accordion from './components/Accordion';
+
+const items = [
+	{
+		title: 'What is React?',
+		content: 'React is a front end javascript framework',
+	},
+	{
+		title: 'Why use React?',
+		content: 'React is a favorite JS library among engineers',
+	},
+	{
+		title: 'How do you use React?',
+		content: 'You use React by creating components',
+	},
+];
 
 function App() {
-  return (
-    <div className="App">
-      hello!
-    </div>
-  );
+	return (
+		<div className='ui container' style={{ marginTop: '10px' }}>
+			<Accordion items={items} />
+		</div>
+	);
 }
 
 export default App;
