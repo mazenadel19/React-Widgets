f8334241a31aec1496c3683696b0bc7df9f4b04c
mazen <mazenadel19@gmail.com>
2021-11-02 22:23:34 +0200

    debouncing input and preventing initial useless search

--- src/components/Translate/Convert.jsx
+++ src/components/Translate/Convert.jsx
@@ -1,33 +1,43 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const Convert = ({ language, text }) => {
 	const [translatedText, setTranslatedText] = useState('')
-	const [debouncedTerm, setDebouncedTerm] = useState(translatedText)
+	const [debouncedTerm, setDebouncedTerm] = useState('')
 
-	useEffect(() => {}, [])
-	useEffect(() => {}, [])
+	useEffect(() => {
+		const timerId = setTimeout(() => {
+			setDebouncedTerm(text)
+		}, 1000)
 
-	// useEffect(() => {
-	// 	console.log('useffect')
-	// 	const doTranslation = async () => {
-	// 		const { data } = await axios.post(
-	// 			'https://translation.googleapis.com/language/translate/v2',
-	// 			{},
-	// 			{
-	// 				params: {
-	// 					q: text,
-	// 					target: language.value,
-	// 					key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
-	// 				},
-	// 			},
-	// 		)
-	// 		setTranslatedText(data.data.translations[0].translatedText)
-	// 	}
-	// 	// doTranslation()
-	// }, [language, text])
+		return () => {
+			clearTimeout(timerId)
+		}
+	}, [language, text])
+	const isFirst = useRef(true)
+	useEffect(() => {
+		if (isFirst.current) {
+			isFirst.current = false
+			return
+		}
+		const doTranslation = async () => {
+			const { data } = await axios.post(
+				'https://translation.googleapis.com/language/translate/v2',
+				{},
+				{
+					params: {
+						q: debouncedTerm,
+						target: language.value,
+						key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+					},
+				},
+			)
+			setTranslatedText(data.data.translations[0].translatedText)
+		}
+		doTranslation()
+	}, [debouncedTerm, language.value])
 
-	return <h1 className="ui header">{translatedText}</h1>
+	return <h1 className='ui header'>{translatedText}</h1>
 }
 
 export default Convert
